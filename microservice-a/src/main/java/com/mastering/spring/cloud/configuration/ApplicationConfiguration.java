ca2ae2d39f322ecde42fc19364f8c86594f81287
leejoonsung <juncheng.li@ucdconnect.ie>
2018-10-11 20:02:37 +0100

    update

--- microservice-a/src/main/java/com/mastering/spring/cloud/configuration/ApplicationConfiguration.java
+++ microservice-a/src/main/java/com/mastering/spring/cloud/configuration/ApplicationConfiguration.java
@@ -4,9 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("application") // Defines a class defining application.properties
+@ConfigurationProperties("application") // Defines a class defining bootstrap.properties
 public class ApplicationConfiguration {
-	private String message; // The value can be configured in application.properties with application.message as key
+	private String message; // The value can be configured in bootstrap.properties with application.message as key
 
 	
 	public String getmessage() {
