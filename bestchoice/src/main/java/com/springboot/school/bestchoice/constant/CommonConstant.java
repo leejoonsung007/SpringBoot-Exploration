e0b83efc582b17713c5fc99ee60a9e6f1deab3b7
leejoonsung <juncheng.li@ucdconnect.ie>
2018-10-29 22:47:30 +0000

    update

--- bestchoice/src/main/java/com/springboot/school/bestchoice/constant/CommonConstant.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.school.bestchoice.constant;
-
-public class CommonConstant {
-
-    public static final String USER_ATTRIBUTE = "loginUser";
-
-    public static final String PLAIN_USER_ATTRIBUTE = "user";
-}
